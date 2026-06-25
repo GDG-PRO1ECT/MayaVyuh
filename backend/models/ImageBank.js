1f0b6468b3688df3e23091a83791b81ff243ee8b
Rajshree Balsamant <rajshreebalsamant@gmail.com>
2026-06-25 19:06:25 +0530

    Updates

--- backend/models/ImageBank.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const imageBankSchema = new mongoose.Schema({
-  url: { type: String, required: true },
-  assignedTeam: { type: mongoose.Schema.Types.ObjectId, ref: 'Team', default: null },
-  used: { type: Boolean, default: false }
-});
-
-module.exports = mongoose.model('ImageBank', imageBankSchema);
--- backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, unique: true },
-  role: { type: String, enum: ['player', 'admin'], default: 'player' },
-});
-
-module.exports = mongoose.model('User', userSchema);
